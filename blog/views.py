dc64df30c0fa1ab7d29880ad3e9bb9abeedb463d
dougiemath <douglasimatheson@gmail.com>
2022-01-26 10:30:42 +0000

    bug fixing committ

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Post
 
+
 class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by("-created_on")
@@ -10,21 +11,21 @@ class PostList(generic.ListView):
 
 
 class PostDetail(View):
+
     def get(self, request, slug, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
-        post = get_object_or_404(queryset,slug=slug)
-        comments = post.comments.filter(approved=True).order_by('created_on')
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by("-created_on")
         liked = False
         if post.likes.filter(id=self.request.user.id).exists():
-            liked=True
-        
+            liked = True
+
         return render(
             request,
-            'post_detail.html',
+            "post_detail.html",
             {
                 "post": post,
                 "comments": comments,
-                "liked": liked,
-            }
+                "liked": liked
+            },
         )
-        
