b2704e1762ca6c006232825dce21fd1d8baafcbf
dougiemath <douglasimatheson@gmail.com>
2022-02-11 12:19:11 +0000

    added views to register.  admin now inaccessible

--- register/views.py
+++ register/views.py
@@ -4,6 +4,12 @@ from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def register(response):
-    form = UserCreationForm()
+    if response.method =="POST":
+        form = UserCreationForm(response.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = UserCreationForm()
+
     return render(response, "register/register.html", {"form":form})
     
